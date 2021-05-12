a06d567abd23eae2b95a60cc29d483c05be6a75b
LucasManoelDuarte <84089272+LucasManoelDuarte@users.noreply.github.com>
2021-05-12 16:14:16 -0300

    Add files via upload

---
+++ quatro-jogos/README.markdown
@@ -0,0 +1,5 @@
+# Quatro jogos
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Lucas_MDuarte/pen/abpddgE](https://codepen.io/Lucas_MDuarte/pen/abpddgE).
+
+
---
+++ quatro-jogos/src/script.js
@@ -0,0 +1,48 @@
+//Funções do BloodBorne (Início)
+var sinopseBlood = "A história de Bloodborne se dá em uma cidade esquecida chamada Yharnam, famosa por seus remédios. Isto faz com que muitos peregrinos a visitem para obter ajuda. No entanto, Yharnam foi atingida por uma doença que transforma humanos em monstros. O protagonista visita o local para tentar a cura, mas encontra apenas pesadelos."
+
+function aparecerSinopseBlood(){
+  document.getElementById("sinopse").innerHTML=sinopseBlood;
+}
+
+function desaparecerSinopseBlood(){
+  document.getElementById("sinopse").innerHTML=" ";
+}
+//Funções BloodBorne (final)
+
+
+//Funções God Of War (Início)
+var sinopseWar = "Kratos, o Deus da Guerra, deixou para trás sua vingança contra os deuses do Olimpo. Agora, vive como um homem comum, refugiado no reino dos deuses e monstros nórdicos. Mais do que lutar pela sua própria sobrevivência, deve ensinar seu filho Atreus a conviver em um mundo hostil e cheio de desafios. Pai e filho empreendem uma jornada empolgante e de incríveis descobertas."
+
+function aparecerSinopseWar(){
+  document.getElementById("sinopse").innerHTML=sinopseWar;
+}
+
+function desaparecerSinopseWar(){
+  document.getElementById("sinopse").innerHTML=" ";
+}
+//Funções God Of War (final)
+
+//Funções Final Fantasy (inicio)
+var sinopseRpg = "A história se passa na metrópole distópica de Midgar e acompanha o mercenário Cloud Strife, que se junta a um grupo ecoterrorista em uma tentativa de impedir que a poderosa megacorporação Shinra use a essência vital do planeta como fonte de energia. A jogabilidade combina combate em tempo real com elementos de estratégia."
+
+function aparecerSinopseRpg(){
+  document.getElementById("sinopse").innerHTML=sinopseRpg;
+}
+
+function desaparecerSinopseRpg(){
+  document.getElementById("sinopse").innerHTML=" ";
+}
+//Funções Final Fantasy (final)
+
+//Funções Tha Last of Us (inicio)
+var sinopseUs = "A história se passa 20 anos após a civilização moderna ser destruída. Joel, um sobrevivente durão, é contratado para contrabandear Ellie, uma jovem de 14 anos, para fora de uma zona de quarentena opressora. O que começa como um pequeno trabalho se torna uma jornada brutal e emocionante, conforme eles atravessam os Estados Unidos e dependem um do outro para sobreviver."
+
+function aparecerSinopseUs(){
+  document.getElementById("sinopse").innerHTML=sinopseUs;
+}
+
+function desaparecerSinopseUs(){
+  document.getElementById("sinopse").innerHTML=" ";
+}
+//Funções The Last of Us (final)
